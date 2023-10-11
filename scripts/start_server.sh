21f024b07df8a06302b52c6146656a83bc1c30a1
SiddhantBodile <siddhantbodile@gmail.com>
2023-10-11 05:51:58 +0000

    start script changed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,8 @@
 # Change to your application directory
 cd /var/www/nodeproject/
 
+# Install pm2 globally
+npm install -g pm2
+
 # Start your Node.js application using pm2
 pm2 start app.js --name nodejs-app
